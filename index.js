79385d3fac732019f0844492ccc656b453954321
MAPXOR12 <84636715+MAPXOR12@users.noreply.github.com>
2021-06-05 23:20:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -249,36 +249,7 @@ ${process.version}
 ///////
 
 
-  client.on('message', message => {
-if (message.content.startsWith(prefix + 'listEmoji')) {
-  let Emojis = "";
-    let EmojisAnimated = "";
-    let EmojiCount = 0;
-    let Animated = 0;
-    let OverallEmojis = 0;
-    function Emoji(id) {
-      return client.emojis.cache.get(id).toString();
-    }
-    message.guild.emojis.cache.forEach((emoji) => {
-      OverallEmojis++;
-      if (emoji.animated) {
-        Animated++;
-        EmojisAnimated += Emoji(emoji.id);
-      } else {
-        EmojiCount++;
-        Emojis += Emoji(emoji.id);
-      }
-    });
-    let Embed = new Discord.MessageEmbed()
-      .setTitle(`Emojis in ${message.guild.name}.`)
-      .setDescription(
-        `**Animated [${Animated}]**:\n${EmojisAnimated}\n\n**Standard [${EmojiCount}]**:\n${Emojis}\n\n**All Emoji [${OverallEmojis}]**`
-      )
-      .setColor(`RANDOM`);
-    message.channel.send(Embed);
- 
-}})
-
+  
 //command Handler DO NOT TOUCH
  const prefixRegex = new RegExp(`^(<@!?${client.user.id}>|${escapeRegex(prefix)})\\s*`);
  if (!prefixRegex.test(message.content)) return;
