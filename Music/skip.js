9decc33752d71ac9b9e62b38d27bf7c971f7e49d
MAPXOR12 <84636715+MAPXOR12@users.noreply.github.com>
2021-06-13 11:21:43 +0300

    Update skip.js

--- Music/skip.js
+++ Music/skip.js
@@ -19,7 +19,7 @@ execute(message) {
     //if not in a guild retunr
     if (!message.guild) return;
     //react with approve emoji
-    message.react("<:emoji_74:815251307635015731>").catch(console.error);
+    message.react("<a:emoji_44:852963983761211443>").catch(console.error);
     //get the queue
     const queue = message.client.queue.get(message.guild.id);
     //if no Queue return error
