f54ee8c7a1799709286be9c9d4ef8b5db7d9efd7
MAPXOR12 <84636715+MAPXOR12@users.noreply.github.com>
2021-06-13 11:24:57 +0300

    Create emoji.js

---
+++ others/emoji.js
@@ -0,0 +1,30 @@
+const { Client, Collection, MessageEmbed } = require(`discord.js`);
+const { 
+  PREFIX, 
+} = require(`../config.json`);
+
+  
+
+
+module.exports = {
+  name: "help",
+  aliases: ["h"],
+  cooldown: 8,
+  description: "**all commands Pinky**",
+  execute(message) {
+    let commands = message.client.commands.array();
+
+    let helpEmbed = new MessageEmbed()
+    .setThumbnail(``)
+    .setImage(``)
+    .setTitle(`Calms Is One`)
+    .setDescription(`
+`)
+
+   .setFooter(`${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
+   .setColor("RANDOM")
+   message.react("<a:emoji_54:853539663389523988>", <a:emoji_34:822641693387653182>")
+    return message.channel.send(helpEmbed).catch(console.error);
+
+  }
+};
