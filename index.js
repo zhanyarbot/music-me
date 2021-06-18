530d7391b6adf220a67cf9686fd66bbff3b8884e
MAPXOR12 <84636715+MAPXOR12@users.noreply.github.com>
2021-06-18 22:38:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -114,11 +114,10 @@ var nick = args.join(" ");
 if(!user || !args) return message.channel.send(`
 \`\`\`js
 Command: setnick
-تغيير لقب العضو.
  
-الاستخدام:
-+setnick (العضو)
-+setnick (العضو) (اللقب الجديد)
+Nickname:
++setnick (name)
++setnick (user) (nick)
  
 \`\`\`
  
@@ -127,6 +126,16 @@ message.guild.member(user.user).setNickname(`${nick}`);
 message.channel.send(`Successfully changed **${user}** nickname to **${nick}**`);
 }
 });
+
+client.on("message", msg => {
+if(msg.content.startsWith(PREFIX +"clear")) {
+ let args = msg.content.split(" ").slice(1);
+ if(!msg.member.hasPermission("MANAGE_MESSAGES")) return msg.channel.send("You can't use this command!");
+        if(!args[0]) return msg.channel.send("Specify how many messages you want to delete.");
+        msg.delete();
+        msg.channel.bulkDelete(args[0]).catch(e => { msg.channel.send("You can only delete 100 messages at once.")});
+        msg.channel.send(`Successfully deleted \`${args[0]} messages\``).then(m => m.delete({ timeout: 5000 }));
+}})
 
 client.on(`ready`, () => {
 
