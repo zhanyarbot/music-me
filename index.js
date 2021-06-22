644e73d19467bad01b34ae0f977f7986949dd700
MAPXOR12 <84636715+MAPXOR12@users.noreply.github.com>
2021-06-22 21:19:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -51,6 +51,41 @@ client.on("message", message => {
   }
 });
 
+        client.on("message"), message => {
+         if (message.content.startsWith(PREFIX + "tinvite")
+        message.guild.fetchInvites().then((invites) => {
+            const inviteCounter = {}
+
+
+            invites.forEach((invite => {
+                const { uses, inviter } = invite
+                const { username, discriminator } = inviter
+
+                const name = `{inviter}`
+
+                inviteCounter[name] = (inviteCounter[name] || 0) + uses
+             }))
+
+             let replyText = new MessageEmbed()
+             .setDescription('**Top invites** \n')
+             .setColor("BLUE")
+
+             const sortedInvites = Object.keys(inviteCounter).sort((a, b) => inviteCounter[b] - inviteCounter[a])
+
+             if (sortedInvites.length > 10) sortedInvites.length = 10
+             else if(sortedInvites.length > 10) sortedInvites.length = sortedInvites.length
+
+
+             for(const invite of sortedInvites) {
+                 const count = inviteCounter[invite]
+                 replyText.description += `\n${invite} has invited ${count} member(s)!`
+             }
+             message.channel.send(replyText)
+
+
+        })
+    }
+
 client.on("message", message => {
   if (!message.channel.guild) return;
   if (message.content.startsWith(PREFIX + "move")) {
