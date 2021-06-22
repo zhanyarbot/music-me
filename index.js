8f6b2dd427a676462e385b4f4c1ee927cc270f03
MAPXOR12 <84636715+MAPXOR12@users.noreply.github.com>
2021-06-22 14:56:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -137,44 +137,6 @@ exports.run = async (app, message, client, args) => {
     }
 
 
-const { MessageEmbed } = require("discord.js")
-exports.run = async (app, message, client, args) => {
-
-
-        message.guild.fetchInvites().then((invites) => {
-            const inviteCounter = {}
-
-
-            invites.forEach((invite => {
-                const { uses, inviter } = invite
-                const { username, discriminator } = inviter
-
-                const name = `{inviter}`
-
-                inviteCounter[name] = (inviteCounter[name] || 0) + uses
-             }))
-
-             let replyText = new MessageEmbed()
-             .setDescription('**Top invites** \n')
-             .setColor("BLUE")
-
-             const sortedInvites = Object.keys(inviteCounter).sort((a, b) => inviteCounter[b] - inviteCounter[a])
-
-             if (sortedInvites.length > 10) sortedInvites.length = 10
-             else if(sortedInvites.length > 10) sortedInvites.length = sortedInvites.length
-
-
-             for(const invite of sortedInvites) {
-                 const count = inviteCounter[invite]
-                 replyText.description += `\n${invite} has invited ${count} member(s)!`
-             }
-             message.channel.send(replyText)
-
-
-        })
-    }
-
-
 const rply = [
 'ڕاوەستە با قوڵتر لە سیمات ڕابمێنم کێ دەزانێ ڕەنگە تاتۆدێیەوە من نەمێنم"];',
 'بەخەیاڵیشمدا نەهاتبو هێندە تەنهابم..',////ba dlli xot la jegai zhmaraka wllami slawaka bnwsa
