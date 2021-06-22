1966ec30e6b265878b054a7efee5fa79720a947c
MAPXOR12 <84636715+MAPXOR12@users.noreply.github.com>
2021-06-22 13:19:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -67,12 +67,37 @@ const rply = [
  'کەسێک لە ژیانما نیە،ژیانم لەکەسێکایە'
 ];
  client.on('message', msg => {
- if (msg.content === "tkurd") {/////ba dlli xot la jegai sllaw shtek da bna
+ if (msg.content === "+tkurd") {/////ba dlli xot la jegai sllaw shtek da bna
    msg.channel.send(`${rply[Math.floor(Math.random() * rply.length)]}`);
   }
 }); 
  
-
+const rply = [
+'Never bend your head. Always hold it high. Look the world straight in the eye. - Helen Keller',
+'A year later you wish you had started today',////ba dlli xot la jegai zhmaraka wllami slawaka bnwsa
+'°It will be morning tonight, be patient°',
+'Life is like riding a bicycle. To keep your balance, you must keep moving. - Albert Einstein',
+'It is during our darkest moments that we must focus to see the light.',
+ 'Believe you can and you re halfway there. - Theodore Roosevelt',
+ 'Some people look for a beautiful place. Others make a place beautiful. - Hazrat Inayat Khan',
+ 'I cant change the direction of the wind.but I can adjust my sails to always reach my destination. - Jimmy Dean',
+ 'Spread love everywhere you go. - Mother Teresa',
+ 'Gods decide is out of our hindsight power but it is always beneficial for us.',
+ 'When you have a dream, youve got to grab it and never let go.',
+ 'When you are not well, memories come to you sooner than anyone else.',
+ 'If you have good thoughts they will shine out of your face like sunbeams and you will always look lovely. - Roald Dahl',
+ 'When you catch in a calumny, you know your real friends.',
+ 'Let us make our future now, and let us make our dreams tomorrows reality. - Malala Yousafzai',
+ 'A successful man is one who can lay a firm foundation with the bricks others have thrown at him',
+ 'In order to succeed, your desire for success should be greater than your fear of failure',
+ 'If size mattered the elephant would be the king of jungle',
+ 'You will reap what you plant in your minds farm'
+];
+ client.on('message', msg => {
+ if (msg.content === "+tenglish") {/////ba dlli xot la jegai sllaw shtek da bna
+   msg.channel.send(`${rply[Math.floor(Math.random() * rply.length)]}`);
+  }
+}); 
              
     
 client.on("message", function(niro_games) {
