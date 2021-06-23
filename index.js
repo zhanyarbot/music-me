57758729b31422722f2a6e3a371a0a27d420b239
MAPXOR12 <84636715+MAPXOR12@users.noreply.github.com>
2021-06-23 16:08:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
- const Discord = require(`discord.js`);
+const Discord = require(`discord.js`);
 const { Client, Collection, MessageEmbed,MessageAttachment } = require(`discord.js`);
 const { readdirSync } = require(`fs`);
 const { join } = require(`path`);
@@ -50,6 +50,32 @@ client.on("message", message => {
       });
   }
 });
+
+client.on("message", message => {
+  if (message.content.startsWith(PREFIX + "mutevoice")) {
+    if (!message.member.hasPermission("MUTE_MEMBERS"))
+      return message.channel
+        .sendMessage("**You dont Have premisssion Mutevoicr** ")
+        .then(m => m.delete(5000));
+    if (!message.guild.member(client.user).hasPermission("MUTE_MEMBERS"))
+      return message
+        .reply("**I Don't Have `MUTE_MEMBERS` Permission**")
+        .then(msg => msg.delete(6000));
+
+    if (message.mentions.users.size === 0) {
+      return message.reply("Menition member");
+    }
+    let muteMember = message.guild.member(message.mentions.users.first());
+    if (!muteMember) {
+      return message.reply("Restart");
+    }
+    muteMember.setMute(true);
+    if (muteMember) {
+      message.channel.sendMessage("Muted voice ");
+    }
+  }
+});
+  
 
 
 client.on("message", message => {
