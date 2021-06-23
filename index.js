68006c9192c4622ca8dc2abac96b5767fe2c5f3b
MAPXOR12 <84636715+MAPXOR12@users.noreply.github.com>
2021-06-23 16:22:10 +0300

    Update index.js

--- index.js
+++ index.js
@@ -73,30 +73,6 @@ client.on("message", message => {
   }
 });
 
-///////
-
-
-client.on('message', message => {
-    if (message.content.startsWith(PREFIX + 'say')) {
-        if (message.member.hasPermission("MANAGE_GUILD")) return message.reply("Sorry You Not Have Premission MANAGE GUILD")
-   var say = message.content.split(" ").slice(1).join(" ");
-    if(!say) return message.reply("**Please Type Message For say**")
-        message.channel.send(say);
-}
-    if (message.content.startsWith(prefix  + "embed")) {
-        if (message.member.hasPermission("MANAGE_GUILD")) return message.reply("Sorry You Not Have Premission MANAGE GUILD")
-   var args = message.content.split(" ").slice(1).join(" ");
-   if(!args) return message.reply("**Please Type Message For say Embed**")
-   const embed = new Discord.MessageEmbed()
-        .setColor('RANDOM')
-        .setThumbnail(message.author.avatarURL())
-        .setAuthor(message.author.username,message.author.avatarURL())
-        .setDescription(args)
-        message.channel.send(embed);
-}
-});
-
-
 client.on("message", message => {
   if (!message.content.startsWith(PREFIX)) return;
   if (!message.channel.guild)
