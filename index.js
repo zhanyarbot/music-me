85f3ac4da74fc7c001c41b2f2e744711048acc07
MAPXOR12 <84636715+MAPXOR12@users.noreply.github.com>
2021-06-23 16:32:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -51,6 +51,39 @@ client.on("message", message => {
   }
 });
 
+client.on('message' , message => {
+      if(message.author.bot) return;
+     
+      if(message.content.startsWith(PREFIX + "rolebc")) {
+        if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+        let args = message.content.split(" ").slice(2);
+     var codes = args.join(' ')
+       
+        if(!codes) {
+          message.channel.send("قم بكتابة الرسالة | !rolebc @everyone message")
+            return;
+        }
+     
+     
+              var role = message.mentions.roles.first();
+                if(!role) {
+                  message.reply("لا توجد رتبة بهذا الاسم")
+                    return;
+                }
+            message.guild.members.filter(m => m.roles.get(role.id)).forEach(n => {
+              n.send(
+              "**" + "السيرفر :" + "\n" +
+              `${message.guild.name}` + "\n" +
+              "المرسل :" + "\n" +
+              `${message.author.tag}` + "\n" +
+              "الرسالة :" + "\n" +
+              `${codes}` + "**"
+              )
+            })
+            message.channel.send(`لقد تم ارسال هذه الرسالة الى ${message.guild.members.filter(m => m.roles.get(role.id)).size} عضو`)
+        }
+    });
+
   client.on("message", message => {
   let args = message.content.split(" ");
   if (args[0] === PREFIX + "hightRole") {
