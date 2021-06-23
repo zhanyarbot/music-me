7c9996747a1e113adab5609990cc160adbab0f12
MAPXOR12 <84636715+MAPXOR12@users.noreply.github.com>
2021-06-23 12:00:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -79,35 +79,6 @@ message.channel.send('Created  channel✅')
 }
 }); 
 
-client.on("message", message => {
-  if (message.content.startsWith(PREFIX + "move")) {
-    let args = message.content.split(" ");
-    let user = message.guild.member(
-      message.mentions.users.first() || message.guild.members.cache.get(args[1])
-    );
-    if (!message.channel.guild || message.author.bot) return;
-    if (!message.guild.member(message.author).hasPermission("MOVE_MEMBERS"))
-      return message.channel.send("Please Check Your Permission");
-    if (!message.guild.member(client.user).hasPermission("MOVE_MEMBERS"))
-      return message.channel.send("Please Check My Permission");
-    if (!message.member.voice.channel)
-      return message.channel.send("Your are not in voice channel");
-    if (!user) return message.channel.send(`**>>> ${prefix}move <@mention or id>`);
-    if (!message.guild.member(user.id).voice.channel)
-      return message.channel.send(
-        `**${user.user.username}** Has not in Voice channel`
-      );
-    message.guild
-      .member(user.id)
-      .voice.setChannel(message.member.voice.channel.id)
-      .then(() => {
-        message.channel.send(
-          `**${user.user.username}** has been moved to **${
-            message.guild.member(message.author).voice.channel.name
-          }**`
-        );
-      });
-  }
 
 
 
