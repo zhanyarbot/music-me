a3992ee5640808851f541773f932a40303c6696a
MAPXOR12 <84636715+MAPXOR12@users.noreply.github.com>
2021-06-25 09:27:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -54,7 +54,21 @@ client.on("message", message => {
  
 
 
-     
+     client.on("message", message => {
+  if (message.content.startsWith(PREFIX + "slots")) {
+    let slot1 = ["🍏", "🍇", "🍒", "🍍", "🍅", "🍆", "🍑", "🍓"];
+    let slots1 = `${slot1[Math.floor(Math.random() * slot1.length)]}`;
+    let slots2 = `${slot1[Math.floor(Math.random() * slot1.length)]}`;
+    let slots3 = `${slot1[Math.floor(Math.random() * slot1.length)]}`;
+    let we;
+    if (slots1 === slots2 && slots2 === slots3) {
+      we = "Win!";
+    } else {
+      we = "Lose!";
+    }
+    message.channel.send(`${slots1} | ${slots2} | ${slots3} - ${we}`);
+  }
+});
  
 
 
