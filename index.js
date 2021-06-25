24299128f6c5ae8f527951810a67153e2e681352
MAPXOR12 <84636715+MAPXOR12@users.noreply.github.com>
2021-06-25 08:44:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -52,6 +52,21 @@ client.on("message", message => {
 });
         
  
+
+client.on('message', message => {
+  const aa = message.content.split(" ").slice(1).join(" ");
+  if(message.content.startsWith(PREFIX + "skin")){
+    if(!aa) return message.reply(`:x:  -  **${prefix}skin <name>**`);
+    var ss = new Discord.RichEmbed()
+    .setTitle(`${aa}'s Skin!`)
+    .setURL(`https://minotar.net/armor/body/${aa}/100.png`)
+    .setThumbnail(`https://minotar.net/avatar/${aa}`)
+    .setImage(`https://minotar.net/armor/body/${aa}/100.png`)
+    .setFooter(`Requested By : ${message.author.tag}`, message.author.avatarURL)
+    message.channel.send(ss);
+  }
+});
+
      
  
 
