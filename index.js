00aa377e4ab9d09beb4c827fd710c6746509bbe8
MAPXOR12 <84636715+MAPXOR12@users.noreply.github.com>
2021-06-28 11:59:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -110,7 +110,7 @@ client.on("message", message => {
     if (!sabotage)
       return message.channel.send(`Please Mention A Member to Kill :warning:`);
     message.channel.send(" ❤️❤️❤️${sabotage").then(msg => {
-      msg.edit(```` │ ❤️🍆🍒${sabotage} │ ``` `);
+      msg.edit(’``` │ ❤️🍆🍒${sabotage} │ ``` ’);
       setTimeout(function() {
         msg.edit('``` │💰❤️❤️ ${sabotage} │ ``` ');
       }, 1000);
