ef7cff1728f9bc10d9dd056f2825b896531313ff
MAPXOR12 <84636715+MAPXOR12@users.noreply.github.com>
2021-07-08 10:32:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -51,30 +51,6 @@ client.on("message", message => {
   }
 }); 
 
-client.on('message',async message => {
-if (message.content.startsWith(PREFIX + 'mix')) {
- 
- let args = message.content.split(" ").slice(1).join(" ");
- 
-if (!args[0]) return message.channel.send("You forgot to mention someone!")
-        if (!args[1]) return message.channel.send("You need to mention someone else!")
- 
-        if (args[0] || args[1]) {
-            var FirstUser = message.mentions.members.first() || message.guild.members.cache.get(args[0])
-            var SecondUser = message.mentions.members.first(-1) || message.guild.members.cache.get(args[1])
- 
-            if (!FirstUser) return message.channel.send(`I couldn't find someone named **${args[0]}**!`)
-            if (!SecondUser) return message.channel.send(`I couldn't find someone named **${args[1]}**!`)
- 
-            if (FirstUser || SecondUser) {
-                const FirstUserSliced = FirstUser.user.username.slice(0, FirstUser.user.username.length / 2)
-                const SecondUserSliced = SecondUser.map(user => { return user.user.username.slice(user.user.username.length / 2) })
-                const SecondUserName = SecondUser.map(user => { return user.user.username })
- 
-                message.channel.send(`${FirstUser.user.username} + ${SecondUserName} = **${FirstUserSliced}${SecondUserSliced}**`)
-            }
-        }
-    }})
 
 client.on('message', async message => {
 	if (message.content === PREFIX + 'test') {
