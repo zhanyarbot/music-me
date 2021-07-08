e6bba86fa6b1e1d2a5471bccff82bd3b05464727
MAPXOR12 <84636715+MAPXOR12@users.noreply.github.com>
2021-07-08 10:28:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -51,11 +51,33 @@ client.on("message", message => {
   }
 }); 
 
-
+client.on('message',async message => {
+if (message.content.startsWith(prefix + 'mix')) {
+ 
+ let args = message.content.split(" ").slice(1).join(" ");
+ 
+if (!args[0]) return message.channel.send("You forgot to mention someone!")
+        if (!args[1]) return message.channel.send("You need to mention someone else!")
+ 
+        if (args[0] || args[1]) {
+            var FirstUser = message.mentions.members.first() || message.guild.members.cache.get(args[0])
+            var SecondUser = message.mentions.members.first(-1) || message.guild.members.cache.get(args[1])
+ 
+            if (!FirstUser) return message.channel.send(`I couldn't find someone named **${args[0]}**!`)
+            if (!SecondUser) return message.channel.send(`I couldn't find someone named **${args[1]}**!`)
+ 
+            if (FirstUser || SecondUser) {
+                const FirstUserSliced = FirstUser.user.username.slice(0, FirstUser.user.username.length / 2)
+                const SecondUserSliced = SecondUser.map(user => { return user.user.username.slice(user.user.username.length / 2) })
+                const SecondUserName = SecondUser.map(user => { return user.user.username })
+ 
+                message.channel.send(`${FirstUser.user.username} + ${SecondUserName} = **${FirstUserSliced}${SecondUserSliced}**`)
+            }
+        }
+    }})
 
 client.on('message', async message => {
 	if (message.content === PREFIX + 'test') {
-   
 		let pages = [
 			
 **Security**
@@ -167,7 +189,7 @@ backwards.on('collect', r => {
 					page++;
 					embed.setDescription(pages[page - 1]);
 					embed.setFooter(`Page ${page} of ${pages.length}`);
-					msg.edit(embed);
+					msg.channel.send(embed);
          });
 			});
 		});
