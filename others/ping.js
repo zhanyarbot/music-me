19c058d249cd42d7e6838b4041bb82fadecc53b2
MAPXOR12 <84636715+MAPXOR12@users.noreply.github.com>
2021-07-10 14:32:56 +0300

    Update ping.js

--- others/ping.js
+++ others/ping.js
@@ -13,7 +13,7 @@ module.exports = {
   edesc: "Type this command to see how fast the Bot can response to your messages / commands inputs!",
   execute(message, args, client) {
     //react with approve emoji
-    message.react("<a:emoji_81:830444292111204382>");
+    message.react("<:2_:863362568801222656>");
     //send the Ping embed
     message.reply(new MessageEmbed().setColor("YELLOW").setTitle("<a:emoji_66:830443189399715860> **" + client.ws.ping + "ms**"));
   }
