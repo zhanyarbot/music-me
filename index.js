2d985dd38b10498f59027e6c7fa635d58c2edcc2
MAPXOR12 <84636715+MAPXOR12@users.noreply.github.com>
2021-07-10 22:00:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -51,6 +51,22 @@ client.on("message", message => {
   }
 }); 
 
+client.on("message", (message) => {
+  if (message.content.startsWith(PREFIX + "create logs")) {
+    if (message.author.id !== message.guild.owner.user.id)
+      return message.channel.send(
+        "Tanha bo owner ship~"
+      );
+      message.guild.id.createChannel("Logs.", "category").then(catg =>{
+      message.guild.id.createChannel("log-pic", "text").then(chat => { chat.setParent(catg.id) })
+      message.guild.id.createChannel("log-message", "text").then(chat => { chat.setParent(catg.id) })
+      message.guild.id.createChannel("log-ban", "text").then(chat => { chat.setParent(catg.id) })
+      message.guild.id.createChannel("log-join", "text").then(chat => { chat.setParent(catg.id) })
+      message.guild.id.createChannel("log-leave", "text").then(chat => { chat.setParent(catg.id) })
+  message.channel.sendMessage(':white_check_mark: **Done Create.**')
+      })
+  }
+  });
 
 client.on('message', async abdo => {
     if (abdo.content.startsWith(PREFIX + "delete channel")) {
