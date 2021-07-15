9ac93840251c9e2e820759e0344668d203c33ef0
MAPXOR12 <84636715+MAPXOR12@users.noreply.github.com>
2021-07-15 12:43:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -52,25 +52,6 @@ client.on("message", message => {
 }); 
 
 
-client.on("message", message => {
-  if (message.content === "+b") {
-    const embed = new Discord.MessageEmbed()
-      .setTitle("بۆ پشاندانی پینگی بۆتەکەت کلیک بکە")
-      .setColor("GOLD");
-    const button1 = new buttonClient.MessageButton()
-      .setLabel("پەنجەی پیانێ مامە ")
-      .setStyle("green")
-      .setID("yes");
-    buttonClient.send(null, {
-      channel: message.channel.id,
-      embed,
-      buttons: [[button1]]
-    });
-  }
-});
-buttonClient.on("yes", inta =>
-  inta.message.reply(`${client.ws.ping}ms بڕۆ خۆتی ڤانە پینگی بۆتەکەت `)
-);
 
 
 client.on('message', async abdo => {
