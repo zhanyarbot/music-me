e2935401b77921e226aa2f1bfc4abf445b8990d5
SHVANx69 <78313191+SHVANx69@users.noreply.github.com>
2021-07-18 17:22:36 +0300

    Update support.js

--- others/support.js
+++ others/support.js
@@ -16,18 +16,18 @@ module.exports = {
 
     let helpEmbed = new MessageEmbed()
     .setThumbnail(``)
-    .setImage(`https://media.discordapp.net/attachments/848815969233403934/850660018532777984/emoji_17.gif `)
-    .setTitle(`CAlms is One`)
+    .setImage(``)
+    .setTitle(`Panda is One`)
     .setDescription(`
-***<a:emoji_17:850611627321327617> OWNER BOT <@743887896481628190> <a:emoji_1:848805449155215391>***
+***<a:emoji_17:850611627321327617> OWNER BOT <@701546840063082601> <a:emoji_1:848805449155215391>***
 <a:emoji_16:850611586921398283>
-<a:emoji_29:850653075055050772>[YOUTUBE](https://youtube.com/channel/UCU7jT6CLQvKIkyEzOuJFUPQ)
+<a:emoji_29:850653075055050772>[YOUTUBE](https://youtube.com/channel/UC4kuqkXtiELDsM9HZDpr-CQ)
 <a:emoji_16:850611586921398283>
-<:g_:850633528697159710> [GITHUP](https://github.com/MAPXOR12)
+<:g_:850633528697159710> [GITHUB](https://github.com/SHVANx69)
 <a:emoji_16:850611586921398283>
-[SERVER SUPPORT](https://discord.gg/mc9kxB9qeM)
+[SERVER SUPPORT](https://discord.gg/progresshome)
 <a:emoji_16:850611586921398283>
-[INVITEBOT](https://discord.com/api/oauth2/authorize?client_id=784303756925468713&permissions=8&scope=bot)`)
+[INVITE](https://discord.com/api/oauth2/authorize?client_id=807350534901071932&permissions=8&scope=bot)`)
 
    .setFooter(`${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
    .setColor("RANDOM")
