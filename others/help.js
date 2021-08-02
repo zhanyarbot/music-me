70ce91a27acf5a7569047a1b746f11ec6e0dc0b0
BlackMan <78313191+xBlackMan69@users.noreply.github.com>
2021-08-02 11:00:11 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -18,62 +18,48 @@ module.exports = {
     let helpEmbed = new MessageEmbed()
     .setThumbnail(``)
     .setImage(``)
-    .setTitle(`Panda Is One`)
+    .setTitle(`Evil Bot Commands`)
     .setDescription(`
 *** | Prefix bot*** 
 ***__>__***
 
-<a:emoji_7:859099940499750912> ***| User Commands***
-
-
+<:emoji_94:852672917031616522> ***| User Commands***
 > invite ・ support(supp)・ uptime ・ prefix
 > ping ・ invites・avatar ・ se ・ 
 > listEmoji ・ user
 
- <a:Work:852558706708054056> ***|Fun commnads***
-
-
-> topinvites(tinvites)・ setnick
-> lock ・ unlock ban ・ unban
-> clear・kick ・channelinfo
-> vkick・addrole ・ roleremove
-> slowmode・invites ・createchannel
-> deletechannel ・benner ・ count
-> deafend ・ undeafend ・rolebc
+<:emoji_80:852672550155976754> ***|Moderation Commands***
+> topinvites(tinvites) ・ setnick
+> lock ・ unlock ・ ban ・ unban
+> clear ・ kick ・ channelinfo
+> vkick ・ addrole ・ roleremove
+> slowmode ・ invites ・ createchannel
+> deletechannel ・ benner ・ count
+> deafend ・ undeafend ・ rolebc
 > hightRole ・ rooms ・
 
 <:emoji_61:863458308219731978> ***| Gif Commands***
-
-
 > boy gif ・ love gif・girl gif ・ sad gif ・ meme ・ 
 > anime
 
-***| photo Commnads ***
-
-
+<:emoji_79:852672528466968578> ***| photo Commnads ***
 > pboy ・ pgirl ・ panime 
 
-***| Game gif Commands***
+<a:emoji_90:842129191301677107> ***| Game Commands***
+> stone ・ scissors ・ kill ・ slots ・ rps
 
+<:emoji_78:870361473107517471> ***| Funny Commands***
+> boom ・ hug ・ paper ・ kiss ・ slap
 
-> boom ・ hug  ・ rps ・ kiss ・ slap ・slots
-
-<a:emoji_20:856127714804105216> ***| Game Commands***
-
-
-> rps ・ slots・stone ・ paper ・ scissors ・ kill
-
-<a:music:852559405718175744> ***| Music Commands***
-
-
-> play ・ skip・lyrics
-> nowplaying・skipto・ stop
-> volume・nowplaying ・ resume
-> shuffle ・ search ・remove
-> queue ・ filter ・ radio・loob
+<:emoji_85:852672706881388564> ***| Music Commands***
+> play ・ skip ・ lyrics
+> nowplaying ・ skipto ・ stop
+> volume ・ nowplaying ・ resume
+> shuffle ・ search ・ remove
+> queue ・ filter ・ radio ・ loob
 
 **Links**
-[SUPPORT](https://discord.gg/progresshome)       [INVITE](https://discord.com/api/oauth2/authorize?client_id=807350534901071932&permissions=8&scope=bot) [website](comming soom)
+[SUPPORT](https://discord.gg/DcP6vw3CRw)       [INVITE](https://discord.com/api/oauth2/authorize?client_id=871657014982508574&permissions=8&scope=bot) [website](comming soom)
 `)
 
    .setFooter(`${message.author.username}#${message.author.discriminator}`, message.member.user.displayAvatarURL({ dynamic: true }))
