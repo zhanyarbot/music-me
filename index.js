1c477a220c39a55412c4b485d8563edc4ee9157b
zhanyarbot <92117702+zhanyarbot@users.noreply.github.com>
2021-10-25 23:08:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = require('quick.db');
 const { TOKEN, PREFIX, AVATARURL, BOTNAME, } = require(`./config.json`);
 const figlet = require("figlet");
 const client = new Client({ disableMentions: `` , partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
-client.login('OTAxMTIwMzAyMzA4Nzg2MjI2.YXLPrg.cmDFc2N2_-kJTEYixuGBVs3sqO8');
+client.login('OTAxMTIwMzAyMzA4Nzg2MjI2.YXLPrg.yg-Re2tSbgqSxp4C-pPPrX12Svo');
 client.commands = new Collection();
 client.setMaxListeners(0);
 client.prefix = PREFIX;
